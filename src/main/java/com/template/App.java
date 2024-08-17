c5bbf33ff04e9538d0daf00befb2bd5c1282152f
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-08-17 18:49:35 -0300

    Update App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,14 +1,30 @@
-package com.template;
-
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-public class App {
-
-
-    public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+public class BankAccount {
+    public Map<String, Float> Account;
+    public final String accountName;
+    public BankAccount(String accountName) {
+        this.Account = new HashMap<>();
+        this.Account.put(accountName, 0f);
+        this.accountName = accountName;
     }
-
-
+    public void deposit(float amount) {
+        if (amount > 0) {
+            float newBalance = this.Account.get(accountName) + amount;
+            Account.put(accountName, newBalance);
+        }
+    }
+    public void withdraw(float amount) {
+        if (amount > 0 && this.Account.get(accountName) > amount) {
+            float newBalance = this.Account.get(accountName) - amount;
+            Account.put(accountName, newBalance);
+        }
+    }
+    public void transfer(BankAccount destination, float amount) {
+        if (amount > 0 && Account.get(accountName) > amount && destination != null  && destination != this) {
+            this.withdraw(amount);
+            destination.deposit(amount);
+        }
+    }
 }
